c57b171959fef52a8614eac117b5fb7fc6b281cf
Agata Budzinska <agatabudzinska@gmail.com>
2018-07-30 20:29:58 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
+        <div className="game-info">
+        </div>
       </div>
     );
   }
