8c8b8ebf8954c113c8bbc5b6cf008fdb5cde37bb
Agata Budzinska <agatabudzinska@gmail.com>
2018-08-01 18:15:56 +0200

    Determination of who is the winner added

--- src/Board.js
+++ src/Board.js
@@ -12,7 +12,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = this.state.xIsNext ? 'x' : 'o'
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
@@ -28,7 +32,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o')
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'The winner is ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
@@ -54,3 +64,23 @@ class Board extends React.Component {
 }
 
 export default Board
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
